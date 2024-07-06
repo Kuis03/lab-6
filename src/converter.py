16dfe357103f3ad51423b7d34c2812882d11dc66
Mateusz Cyran <mateusz.cyran13@gamil.com>
2024-07-06 16:29:13 +0200

    Dodanie JSON

--- src/converter.py
+++ src/converter.py
@@ -10,3 +10,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+import json
+
+def load_json(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            
+            return data
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON syntax in file '{filename}'.")
+        return None
+
+data = load_json('pathFile1.json')
+if data:
+    print(data) 
